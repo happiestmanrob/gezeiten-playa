5c62c371832bb440bbb8b808cd491e41c6b991f9
happiestmanrob <happiestmanrob@gmail.com>
2025-10-17 11:55:47 +0200

    Update fetch.js

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -3,7 +3,8 @@
 import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import cheerio from "cheerio";
+import * as cheerio from "cheerio";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
